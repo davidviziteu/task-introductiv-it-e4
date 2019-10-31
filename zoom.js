9d5a1921f972d36749571685ffd5a1c6cf6fa232
David Viziteu <andrei_david7@hotmail.com>
2019-10-31 20:45:30 +0200

    jgv

---
+++ zoom.js
@@ -0,0 +1,25 @@
+$(document).keydown(function(event) {
+    if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+            event.preventDefault();
+         }
+        // 107 Num Key  +
+        // 109 Num Key  -
+        // 173 Min Key  hyphen/underscor Hey
+        // 61 Plus key  +/= key
+    });
+    
+    $(window).bind('mousewheel DOMMouseScroll', function (event) {
+           if (event.ctrlKey == true) {
+           event.preventDefault();
+           }
+    });
+
+
+
+setInterval(function(){
+    var scale = 'scale(1)';
+document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+document.body.style.msTransform =   scale;       // IE 9
+document.body.style.transform = scale;     // General
+    
+}, 50)
